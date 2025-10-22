ba9f727f1eb41643045fb75f03d848d87f48f31f
Jack31098 <jack.zhou005@gmail.com>
2025-10-22 19:44:22 -0400

    update

--- fsdp/launch_fsdp.sh
+++ fsdp/launch_fsdp.sh
@@ -1,17 +1,17 @@
 cd "$(dirname "$0")"
-export HIP_VISIBLE_DEVICES=0,1
+export HIP_VISIBLE_DEVICES=0,1,2,3
 export MASTER_ADDR=127.0.0.1
 export MASTER_PORT=29500
 export OMP_NUM_THREADS=24
 
 #  --pack_sequences \
 #     --bf16 \
-torchrun --nproc_per_node=2 train_fsdp.py \
-  --model_name_or_path ./models/qwen3-0.6b-base \
-  --train_json data/sft_en.jsonl \
+torchrun --nproc_per_node=4 train_fsdp.py \
+  --model_name_or_path ../models/qwen3-8b-base \
+  --train_json ../data/sft_en.jsonl \
   --seq_len 512 --lr 1e-5 --wd 0.1 \
   --bf16 \
   --use_activation_checkpointing \
   --attn_impl eager \
-  --micro_batch_size 16 --grad_accum_steps 2 --max_steps 48 \
+  --micro_batch_size 2 --grad_accum_steps 2 --max_steps 48 \
   --log_every 8 --output_dir runs/fsdp_s1024_ga50
